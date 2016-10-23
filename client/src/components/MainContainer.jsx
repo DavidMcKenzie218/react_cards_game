df8cf721db2d5bdba581639742465c4da26c7958
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-10-23 17:18:04 +0100

    Cards are dealt to the player and ai

--- client/src/components/MainContainer.jsx
+++ client/src/components/MainContainer.jsx
@@ -5,22 +5,22 @@ const ChosenCards = require('./ChosenCards.jsx');
 const PlayerHand = require('./PlayerHand.jsx');
 const Game = require('../models/game.js');
 
-const testCards = [{desc: "Card 1"}, {desc: "Card 2"}];
-
-
+const testCards = [{desc: "Card 1"}, {desc: "Card 2"}, {desc: "Card 3"}, {desc: "Card 4"}, {desc: "Card 5"}, {desc: "Card 6"}];
 const MainContainer = React.createClass({
 
   getInitialState: function(){
 
-    return{cards: testCards, selectedCard: null, playerChosenCard: null, turnPlaying: false};
+    return{cards: testCards, selectedCard: [{desc: "Selected Card"}], playerChosenCard: null, turnPlaying: false, turn: 0, playerCount: 2, playerCards: [], aiCards: []};
 
   },
 
   onCardClickHandler: function(card){
 
-    let playerCard = {desc: card}
-
-    this.setState({playerChosenCard: [playerCard]});
+    if(this.state.turn != 0){
+      let playerCard = {desc: card};
+    
+        this.setState({playerChosenCard: [playerCard]});
+    }
 
   },
 
@@ -29,12 +29,30 @@ const MainContainer = React.createClass({
   },
 
   onChoiceCardClicked: function(card){
-    console.log("choice " + card + " has been selected")
+    console.log("choice " + card + " has been selected");
+    if(this.state.turn === 0){
+
+      let chosenCard = {desc: card};
+
+      console.log(chosenCard);
+
+    }
   },
 
-  onButtonClickHandler: function(){
-    console.log("button has been clicked");
+  nextTurn: function(){
+    this.setState({turn: (this.state.turn =+ 1), turnPlaying: false})
+    console.log(this.state.turn)
+  },
 
+  playerIsCzar: function(){
+    if(!this.state.turnPlaying){
+      this.setState({selectedCard: this.state.playerChosenCard, turnPlaying: true})
+
+      console.log(this.state.selectedCard);
+    }
+  },
+
+  playerSelectCard: function(){
     if(!this.state.turnPlaying){
       this.setState({selectedCard: this.state.playerChosenCard, turnPlaying: true});
 
@@ -42,10 +60,42 @@ const MainContainer = React.createClass({
       let index = hand.findIndex(this.findChosenCard);
       let newHand = hand.splice(index, 1);
     }
+
+  },
+
+  onButtonClickHandler: function(){
+    console.log("button has been clicked");
+    console.log(this.state.turn);
+
+    if(this.state.turn === 0){
+      this.playerIsCzar();
+      this.nextTurn();
+    }else{
+      this.playerSelectCard();
+    }
   },
 
   findChosenCard: function(card){
     return card.desc === this.state.playerChosenCard[0].desc;
+  },
+
+  dealCards: function(){
+    let playerHand = [];
+    let aiHand = [];
+    for(var i = 0; i < this.state.cards.length; i++){
+      if(i%2){
+        playerHand.push(this.state.cards[i]);
+      }else{
+        aiHand.push(this.state.cards[i])
+      }
+    }
+    this.setState({playerHand: playerHand, aiHand: aiHand})
+  },
+
+  componentWillMount: function(){
+    if(this.state.turn === 0){
+      this.dealCards();
+    }
   },
 
   render: function(){
@@ -56,7 +106,7 @@ const MainContainer = React.createClass({
         <BlackCard onClicked={this.onBlackClicked}/>
         <ChoiceButton onClickHandler={this.onButtonClickHandler}/>
         <ChosenCards cards={this.state.selectedCard} onClicked={this.onChoiceCardClicked}/>
-        <PlayerHand cards={this.state.cards} onClicked={this.onCardClickHandler}/>
+        <PlayerHand cards={this.state.playerHand} onClicked={this.onCardClickHandler}/>
       </div>
       ) 
 
